bca2b9864270af550361b473f3dc907b5df15c0e
Kevin Gibbons <bakkot@gmail.com>
2024-06-08 13:39:35 -0700

    switch to web worker

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,5 +1,3 @@
-import { parse } from '../component-built/component.js';
-
 function debounce(f: (...args: unknown[]) => void, ms: number) {
   let timeout: number | null;
   return function (this: unknown, ...args: unknown[]) {
@@ -15,7 +13,7 @@ function debounce(f: (...args: unknown[]) => void, ms: number) {
 
 // TODO replace this function once https://github.com/tc39/proposal-arraybuffer-base64 ships
 function base64(bytes: Uint8Array) {
-  // @ts-expect-error it's fine
+  // @ts-expect-error it's fine to pass a Uint8Array here
   return btoa(String.fromCharCode.apply(null, bytes));
 }
 
@@ -23,21 +21,48 @@ let input: HTMLTextAreaElement = document.querySelector('#input')!;
 let output: HTMLTextAreaElement = document.querySelector('#output')!;
 let downloadButton: HTMLButtonElement = document.querySelector('#download')!;
 
+let worker = new Worker('worker.js', { type: 'module' });
+
 let lastBytes: Uint8Array;
-function update() {
-  try {
-    lastBytes = parse(input.value);
-    output.value = base64(lastBytes);
-    downloadButton.disabled = false;
-  } catch (e) {
-    (window as any).error = e;
-    output.value = (e as Error).message;
-    downloadButton.disabled = true;
-    return;
+let sent = '';
+let statusMessageTimeout: number | null = null;
+
+// we can't just postmessage to the worker right away because it has a top-level await
+// so we need to wait for it to tell us it's ready
+worker.addEventListener('message', () => {
+  worker.addEventListener('message', gotMessage);
+  update();
+}, { once: true });
+
+function gotMessage({ data }: { data: { success: true, bytes: Uint8Array} | { success: false, error: string }}) {
+  if (statusMessageTimeout != null) clearTimeout(statusMessageTimeout);
+  if (input.value === sent) {
+    if (data.success) {
+      lastBytes = data.bytes;
+      output.value = base64(lastBytes);
+      downloadButton.disabled = false;
+    } else {
+      output.value = data.error;
+      downloadButton.disabled = true;
+    }
+  } else {
+    // the user has already moved on, ignore this result and re-run
+    sent = input.value;
+    worker.postMessage(sent);
   }
 }
 
-update();
+function update() {
+  sent = input.value;
+  worker.postMessage(input.value);
+
+  // wait a bit before updating the message to avoid rapid changes when the output can be parsed quickly
+  statusMessageTimeout = setTimeout(() => {
+    output.value = 'working...';
+    downloadButton.disabled = true;
+  }, 200);
+}
+
 input.addEventListener('input', debounce(update, 100));
 
 downloadButton.addEventListener('click', () => {
@@ -49,5 +74,3 @@ downloadButton.addEventListener('click', () => {
   link.click();
   URL.revokeObjectURL(url);
 });
-
-(globalThis as any).parse = parse;
